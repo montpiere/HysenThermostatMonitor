633a2cb18048989bc286e336016056d29035da0f
bendez <montpiere@gmail.com>
2024-11-24 19:07:05 +0100

    First commit

---
+++ get_data.py
@@ -0,0 +1,53 @@
+import broadlink
+from broadlink.climate import hysen
+
+# Eszközök keresése
+import broadlink
+
+# Eszköz keresése
+devices = broadlink.discover(discover_ip_address='192.168.0.255')
+
+# Első eszköz hitelesítése
+device = devices[0]
+print(device)
+device.auth()
+
+# Hőmérséklet lekérdezése
+data = device.get_full_status()
+
+
+# Adatok formázása és kiírása
+def format_data(data):
+    print("=== Termosztát adatok ===\n")
+    print(f"Szoba hőmérséklet: {data['room_temp']}°C")
+    print(f"Beállított hőmérséklet (Thermostat): {data['thermostat_temp']}°C")
+    print(f"Külső érzékelő hőmérséklete: {data['external_temp']}°C")
+    print(f"Üzemmód: {'Fűtés' if data['active'] != 0 else 'Kikapcsolva'}")
+    print(f"Áramellátás: {'Be' if data['poweron'] else 'Ki'}")
+    print(f"Automata mód: {'Engedélyezett' if data['auto_mode'] > 0 else 'Letiltott'}")
+    print(f"Idő: {data['hour']:02}:{data['min']:02}:{data['sec']:02}, A hét napja: {data['dayofweek']}\n")
+
+    print("=== Hétköznapi beállítások ===")
+    for i, setting in enumerate(data['weekday']):
+        print(
+            f"{i+1}. időszak: {setting['start_hour']:02}:{setting['start_minute']:02} - "
+            f"{setting['temp']}°C"
+        )
+
+    print("\n=== Hétvégi beállítások ===")
+    for i, setting in enumerate(data['weekend']):
+        print(
+            f"{i+1}. időszak: {setting['start_hour']:02}:{setting['start_minute']:02} - "
+            f"{setting['temp']}°C"
+        )
+
+    print("\n=== Egyéb beállítások ===")
+    print(f"Maximális hőmérséklet: {data['svh']}°C")
+    print(f"Minimális hőmérséklet: {data['svl']}°C")
+    print(f"Hőmérséklet különbség: {data['dif']}°C")
+    print(f"Érzékelő mód: {data['sensor']}")
+    print(f"Távoli zárolás: {'Aktív' if data['remote_lock'] else 'Inaktív'}")
+
+
+# Adatok kiírása
+format_data(data)
---
+++ test_data.py
@@ -0,0 +1,37 @@
+data = {
+    'remote_lock': 0,
+    'power': 1,
+    'active': 0,
+    'temp_manual': 0,
+    'heating_cooling': 0,
+    'room_temp': 23.6,
+    'thermostat_temp': 23.0,
+    'auto_mode': 3,
+    'loop_mode': 3,
+    'sensor': 0,
+    'osv': 42,
+    'dif': 2,
+    'svh': 25,
+    'svl': 15,
+    'room_temp_adj': 0.0,
+    'fre': 0,
+    'poweron': 1,
+    'unknown': 2,
+    'external_temp': 42.1,
+    'hour': 18,
+    'min': 7,
+    'sec': 51,
+    'dayofweek': 7,
+    'weekday': [
+        {'start_hour': 3, 'start_minute': 30, 'temp': 23.5},
+        {'start_hour': 6, 'start_minute': 0, 'temp': 22.5},
+        {'start_hour': 11, 'start_minute': 30, 'temp': 22.5},
+        {'start_hour': 14, 'start_minute': 0, 'temp': 23.5},
+        {'start_hour': 18, 'start_minute': 0, 'temp': 23.0},
+        {'start_hour': 22, 'start_minute': 0, 'temp': 22.5}
+    ],
+    'weekend': [
+        {'start_hour': 8, 'start_minute': 0, 'temp': 22.0},
+        {'start_hour': 23, 'start_minute': 0, 'temp': 15.0}
+    ]
+}
